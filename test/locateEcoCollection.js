3a6ed23a1a7d9161a10a47d8e3db41918348535a
Guest_Admin <guest_admin@Ambers-MacBook-Pro.local>
2023-05-15 10:05:59 +0200

    updated locatEcoCollection test to pull correct tag element using '.home-eco'

--- test/locateEcoCollection.js
+++ test/locateEcoCollection.js
@@ -9,16 +9,17 @@ const assert = require('chai').assert;
     I can stay dry and move with ease/comfort.
 */
 
-describe('Locate the Eco-Friendly collection on the Luma Dermostore', () => {
+describe.only('Locate the Eco-Friendly collection on the Luma Dermostore', () => {
     context('I click on the link that will take me to the collection', async () => {
         it('I should be able to view the entire Eco-Friendly collection', async () => {
             const driver = await new Builder().forBrowser('firefox').build();
             try {
                 await driver.get('https://magento.softwaretestingboard.com');
-                await driver.wait(until.elementLocated(By.css('.action.more.icon:last-child')), 10000);
-                await driver.findElement(By.css('.action.more.icon:last-child')).click();
+                //await driver.wait(until.elementLocated(By.css('.action.more.icon:last-child')), 10000);
+                //await driver.findElement(By.css('.action.more.icon:last-child')).click();
                 //let collectionLinks = await driver.findElement(By.css('.action.more.icon'));
                 //await collectionLinks[4].click();
+                await driver.findElement(By.css('.home-eco')).click();
 
                 await driver.sleep(3000);
 
