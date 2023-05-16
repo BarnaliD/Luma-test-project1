b8bf02d0f8e0fe6924d6a3122feba2793ad8cbfa
Guest_Admin <guest_admin@Ambers-MacBook-Pro.local>
2023-05-16 10:24:11 +0200

    fixed and renamed addItemToWishList2 (now addItemToWishListHover) to properly locate wish list icon using hover

--- test/addItemToWishListHover.js
+++ test/addItemToWishListHover.js
@@ -12,7 +12,7 @@ let pass = process.env.PASS;
     list to reference later or share with others.
 */
 
-describe.only("Add an item to the wish list", () => {
+describe("Add an item to the wish list", () => {
     context("Log into the Luma Demostore to add items to wish list", async () => {
         it("I should be able to view item on my wish list ", async () => {
             const driver = await new Builder().forBrowser("firefox").build()
@@ -24,7 +24,6 @@ describe.only("Add an item to the wish list", () => {
               await driver.findElement(By.css('.action.more.button')).click();
 
               // Select an item to add to wish list
-              //await driver.executeScript('document.querySelector("a[href$="/nadia-elements-shell.html"]").scrollIntoView()');
               await driver.wait(until.elementLocated(By.css('a[href$="/echo-fit-compression-short.html"]')), 10000);
               let hoverItem = await driver.findElement(By.css('a[href$="/echo-fit-compression-short.html"]'));
               await driver.executeScript("arguments[0].scrollIntoView(true);", hoverItem);
@@ -32,46 +31,24 @@ describe.only("Add an item to the wish list", () => {
               // Hover over the item
               await actions.move({duration: 1000, origin: hoverItem}).perform();
 
-              // Hover over the item size
+              // Select the item size
               await driver.wait(until.elementLocated(By.id('option-label-size-143-item-171')), 10000);
-              //let hoverSize = await driver.findElement(By.id('option-label-size-143-item-171'));
-              //await actions.move({duration: 1000, origin: hoverSize}).perform();
               await driver.findElement(By.css('#option-label-size-143-item-171')).click();
             
-              // Right-click on the hoverSize
-              //await actions.contextClick(hoverSize).perform();
-            
-              // Hover over the item color
+              // Select the item color
               await driver.wait(until.elementLocated(By.css('#option-label-color-93-item-50')), 10000);
-              //let hoverColor = await driver.findElement(By.css('#option-label-color-93-item-50'));
-              //await actions.move({duration: 1000, origin: hoverColor}).perform();
               await driver.findElement(By.css('#option-label-color-93-item-50')).click();
 
-             // Right-click on the hoverSize
-               //await actions.contextClick(hoverColor).perform();
-            
-             /* // Select item size and color
-              await driver.wait(until.elementLocated(By.css('#option-label-size-143-item-166')), 10000);
-              await driver.findElement(By.css('#option-label-size-143-item-166')).click();
-
-              await driver.wait(until.elementLocated(By.css('#option-label-color-93-item-57')), 10000);
-              await driver.findElement(By.css('#option-label-color-93-item-57')).click(); */
-
               // Add item to wish list
-              //await driver.wait(until.elementLocated(By.css('a[href$="#"]')), 10000);
-              //let hoverWishList = await driver.findElement(By.css('a[href="#"]')).click();
-              //await actions.move({duration: 1000, origin: hoverWishList}).perform();
-              //await actions.contextClick(hoverWishList).perform();
-              //await driver.findElement(By.css('a[href$="#"]')).click();
-              await driver.wait(until.elementLocated(By.css('a.action.towishlist')), 10000);
-              let hoverWishList = await driver.findElement(By.css('a.action.towishlist'));
-              await driver.executeScript("arguments[0].scrollIntoView(true);", hoverWishList);
-              await actions.move({duration: 1000, origin:hoverWishList}).perform();
-              await driver.findElement(By.css('a.action.towishlist')).click();
+              await driver.wait(until.elementLocated(By.css('.action.towishlist')), 10000);
+
+              // Hover over item again in order to make the wish list icon viewable
+              await actions.move({duration: 1000, origin: hoverItem}).perform();
+              await driver.findElement(By.css('.action.towishlist')).click();
 
               await driver.sleep(3000);
 
-              /*// Log into website
+              // Log into website
               
               await driver.findElement(By.css(".authorization-link > a:nth-child(1)")).click();
 
@@ -87,13 +64,12 @@ describe.only("Add an item to the wish list", () => {
 
               await driver.sleep(1000);
 
-
               // View wish list and confirm that item has been added
-              let addedItem = await driver.findElement(By.css('.product-item-name > a[title="Nora Practice Tank"]')).getText();
+              let addedItem = await driver.findElement(By.css('.product-item-name > a[title="Echo Fit Compression Short"]')).getText();
               console.log(addedItem);
-              addedItem.should.contain('Nora Practice Tank'); */
+              addedItem.should.contain('Echo Fit Compression Short'); 
             } finally {
-              //await driver.quit();
+              await driver.quit();
             }
           });
       });
